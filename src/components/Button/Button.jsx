22084ddc8cd1d304dd263c6cf4f72a45bcc404eb
Takhir <tahaha00@mail.ru>
2024-08-12 12:06:57 +0500

    git third branch change

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import './Button.css'
 export default function Button() {
 	return(
-		<button className="button accent">git second change branch</button>
+		<button className="button accent">git third change branch</button>
 	)
 }
 
