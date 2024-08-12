dd735d3dba90939c5c620d3c1f06f0d88967a354
Takhir <tahaha00@mail.ru>
2024-08-12 11:56:30 +0500

    git проверка

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import './Button.css'
 export default function Button() {
 	return(
-		<button className="button accent">git изменен</button>
+		<button className="button accent">git проверка</button>
 	)
 }
 
